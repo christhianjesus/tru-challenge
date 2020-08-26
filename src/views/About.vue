5ade14595edc7b516c0e33c277d1905b806b38e2
Christhian Guevara <christhianjesus@gmail.com>
2020-08-26 18:20:55 -0500

    Super simple views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hi, Everyone.</h1>
+    <p>This page is a test to assess my skills with VueJs.</p>
+    <p>My name is Christhian Guevara, nice to meet you.</p>
   </div>
 </template>
---
+++ src/views/Post.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="post">
+    <div class="post-body">
+      <h2>{{ title }}</h2>
+      <p>{{ msg }}</p>
+      <br />
+      {{ author }}
+    </div>
+    <post-list :posts="posts" />
+    <answer-form class="center" v-if="true" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AnswerForm from "@/components/AnswerForm.vue";
+import PostList from "@/components/PostList.vue";
+
+export default {
+  name: "Post",
+  components: {
+    PostList,
+    AnswerForm
+  },
+  data() {
+    return {
+      title: "Some title",
+      msg: "Some message",
+      author: "Arthur"
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.post-body {
+  margin: 60px;
+  padding: 30px;
+  text-align: left;
+  background-color: white;
+  border: 1px solid #cdcdcd;
+}
+</style>
